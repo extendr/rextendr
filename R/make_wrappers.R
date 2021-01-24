56ef08388c80cf5a31cf837a599fe0f23dc21809
Claus Wilke <wilke@austin.utexas.edu>
2021-01-24 17:20:50 -0600

    some notes for the future

---
+++ R/make_wrappers.R
@@ -0,0 +1,10 @@
+# this file will eventually contain code to read wrapper info from a Rust
+# library and write to a desired location.
+
+# some relevant notes:
+# to get the name of the current package, use code such as the following:
+# library(desc)
+# library(here)
+#
+# x <- desc(file = here("DESCRIPTION"))
+# x$get("Package")
