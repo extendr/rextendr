1f536f901dd465e58b2ca2bdb55025879e439e3f
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-09 13:09:18 +0200

    Improving serialization

--- R/toml_serialization.R
+++ R/toml_serialization.R
@@ -1,15 +1,25 @@
-to_toml <- function(...) {
-    args <- list2(...) %T>% print
+to_toml <- function(
+    ...,
+    .str_as_literal = TRUE,
+    .format_int = "%d",
+    .format_dbl = "%g"
+) {
+    args <- list2(...)
     map2(names2(args), args, function(nm, a) {
         vec_c(
             if (nzchar(nm)) paste0("[", nm, "]") else character(0),
-            format_toml(a)
+            format_toml(
+                a,
+                .str_as_literal = .str_as_literal,
+                .format_int = .format_int,
+                .format_dbl = .format_dbl
+            )
         )
     })
 }
 
 get_toml_err_msg <- function() "Object cannot be serialzied."
-
+get_toml_size_err_msg <- function() "  x `x` has length of `0`.\n  i Input should be of length >= 1.\n"
 format_toml <- function(x, ...) UseMethod("format_toml")
 
 format_toml.default <- function(x, ...)
@@ -17,24 +27,49 @@ format_toml.default <- function(x, ...)
         glue("{get_toml_err_msg()}\n  x `{vec_ptype_abbr(x)}` cannot be converted to toml.\n"),
         call. = FALSE)
 
-format_toml.character <- function(x, ...) {
+format_toml_atomic <- function(x, ..., .formatter) {
     if (length(x) == 0L) {
         stop(
-            glue("{get_toml_err_msg()}\n  x `x` has length of `0`.\n  i Input should be of length >= 1.\n"),
+            paste(get_toml_err_msg(), get_toml_size_err_msg(), sep = "\n"),
             call. = FALSE
         )
     } else {
-        items <- paste0("\"", x, "\"", collapse = ", ")
-        if (length(x) == 1L) {
+        formatter <- as_function(.formatter)
+        items <- paste0(formatter(x, ...), collapse = ", ")
+        if (length(x) > 1L) {
             items <- paste0("[ ", items, " ]")
         }
         items
 
         # debug
-        vec_assert(items, ptype = character(),  size = 1L)
+        vec_assert(items, ptype = character(), size = 1L)
     }
-
 
 }
 
-to_toml(vec_c(1L, 2L, 3L), patch.crates_io = "patch") %>% print
+# This should escape basic symbols
+escape_dbl_quotes <- function(x) {
+    stringi::stri_replace_all_regex(x, "([\"])", r"(\\$1)")
+}
+
+format_toml.character <- function(x, .str_as_literal = TRUE, ...) {
+    if (isTRUE(.str_as_literal)) {
+        .formatter <- ~paste0("'", .x, "'")
+    }
+    else {
+        .formatter <- ~paste0("\"", escape_dbl_quotes(.x), "\"")
+    }
+    format_toml_atomic(x, ..., .formatter = .formatter)
+}
+
+format_toml.integer <- function(x, .format_int = "%d", ...) {
+    format_toml_atomic(x, ..., .format_int = .format_int, .formatter = ~sprintf(.format_int, .x))
+}
+
+format_toml.double <- function(x, .format_dbl = "%g", ...) {
+    format_toml_atomic(x, ..., .format_dbl = .format_dbl, .formatter = ~sprintf(.format_dbl, .x))
+}
+
+
+
+to_toml(vec_c(1L, 2L, 3), patch.crates_io = "Tom \"Dubs\" Preston-Werner") %>% walk(cat, sep ="\n")
