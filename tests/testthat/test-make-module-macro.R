a5f1630f32fe258e6af9167bb8d92153dfcbef25
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-16 23:47:16 +0200

    More simple tests

--- tests/testthat/test-make-module-macro.R
+++ tests/testthat/test-make-module-macro.R
@@ -85,3 +85,70 @@ test_that("Macro generation fails on invalid comments in code", {
  i This error may be caused by a code fragment like `\\*/ ... /\\*`.",
   )
 })
+
+
+test_that("Rust code cleaning", {
+  expect_equal(
+    fill_block_comments(c(
+      "Nested /*/* this is */ /*commented*/ out */",
+      "/*/*/**/*/*/comments."
+    )),
+    c(
+      "Nested                                     ",
+      "            comments."
+    )
+  )
+
+  expect_equal(
+    remove_line_comments("This is visible //this is not."),
+    "This is visible "
+  )
+
+  expect_equal(
+    clean_rust_code(c(
+      "/* Comment #1 */",
+      "   // Comment #2",
+      "              ",
+      " /* Comment #3 //   */"
+    )),
+    character(0)
+  )
+})
+
+test_that("Rust metadata capturing", {
+  expect_equal(
+    find_extendr_attrs_ids(c(
+      "#1",
+      "#[extendr]",
+      "    # 3  ",
+      " #\t [ \textendr   ]",
+      "#5"
+    )),
+    c(2L, 4L)
+  )
+
+  expect_equal(
+    extract_meta("#[extendr] pub \tfn\t      test_fn  \t() {}"),
+    tibble::tibble(
+      match = "fn\t      test_fn",
+      fn = "fn",
+      impl = NA_character_,
+      lifetime = NA_character_,
+      name = "test_fn"
+    )
+  )
+
+  expect_equal(
+    extract_meta(c(
+      "#[extendr]",
+      "pub impl  <'a, \t 'b>  X   <a', 'b> {}"
+    )),
+    tibble::tibble(
+      match = "impl  <'a, \t 'b>  X",
+      fn = NA_character_,
+      impl = "impl",
+      lifetime = "'a, \t 'b",
+      name = "X"
+    )
+  )
+})
