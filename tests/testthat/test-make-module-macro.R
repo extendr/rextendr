e69a67cf11c351c3e30dfc9da7f9be733d9ce6e1
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-02-16 15:54:50 +0200

    More metadta tests

--- tests/testthat/test-make-module-macro.R
+++ tests/testthat/test-make-module-macro.R
@@ -59,10 +59,32 @@ impl Counter {
   )
 })
 
-test_that("Macro generation fails", {
+test_that("Macro generation fails on invalid rust code", {
   expect_error(
     make_module_macro("#[extendr]\nlet invalid_var = ();"),
     "Rust code contains invalid attribute macros.
- x No valid `fn` or `impl` block found in the following sample:"
+ x No valid `fn` or `impl` block found in the following sample:
+ `#\\[extendr\\]
+  let invalid_var = \\(\\);`"
+  )
+})
+
+test_that("Macro generation fails on invalid comments in code", {
+  expect_error(
+    make_module_macro("/*/*/**/"),
+    "Malformed comments.
+ x Number of start `/\\*` and end `\\*/` delimiters are not equal.
+ i Found `3` occurence\\(s\\) of `/\\*`.
+ i Found `1` occurence\\(s\\) of `\\*/`."
+  )
+
+  expect_error(
+    make_module_macro("*/  /*"),
+    paste(
+      "Malformed comments.",
+       "x `/\\*` and `\\*/` are not paired correctly.",
+       "i This error may be caused by a code fragment like `\\*/ ... /\\*`.",
+      sep = "\n "
+    )
   )
 })
