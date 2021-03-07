375d42821257666aea7ab432a4c64221ccbc23bb
Hiroaki Yutani <yutani.ini@gmail.com>
2021-03-07 15:00:08 +0900

    Import glue_collapse()

--- R/rextendr.R
+++ R/rextendr.R
@@ -5,7 +5,7 @@
 #' @name rextendr
 #' @importFrom dplyr   mutate %>%
 #' @importFrom purrr   map2 map2_chr map_lgl flatten_chr map_if every map discard
-#' @importFrom glue    glue
+#' @importFrom glue    glue glue_collapse
 #' @importFrom rlang   dots_list names2 as_function is_missing is_atomic is_null
 #' @importFrom rlang   is_na .data .env
 #' @importFrom stringi stri_replace_all_regex
