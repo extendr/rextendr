d1ab233ace97c3ec5c98827e023cb946dc7a4a09
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-12 20:07:16 +0300

    rextendr::document prototype

---
+++ R/rextendr_document.R
@@ -0,0 +1,13 @@
+#' @export
+document <- function(pkg = ".", quiet = FALSE, roclets = NULL) {
+  needs_compilation <- needs_compilation(pkg, quiet) || pkgbuild::needs_compile(pkg)
+
+  pkgload::load_all(
+    path = pkg,
+    compile = needs_compilation,
+    quiet = quiet
+  )
+
+  register_extendr(path = pkg, quiet = quiet)
+  devtools::document(pkg = pkg, roclets = roclets, quiet = FALSE)
+}
