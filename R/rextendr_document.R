12db53065e45a570c925aff3d3b027bbd3575cbf
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-13 01:39:48 +0300

    Updating docs

--- R/rextendr_document.R
+++ R/rextendr_document.R
@@ -1,3 +1,11 @@
+#' This function is a wrapper for `devtools::document`.
+#' It executes `extendr`-specific routine before calling `devtools::document`,
+#' ensuring Rust code is recompiled when necessary and appropriate R wrappers are generated.
+#'
+#' @param pkg String scalar pointing to the package root.
+#' @param quiet Logical scalar indicating wether the output should be verbose.
+#' @param roclets Additional roclets from `roxygen2` that are passed into
+#'   `roxygen2::document`.
 #' @export
 document <- function(pkg = ".", quiet = FALSE, roclets = NULL) {
   try_save_all()
