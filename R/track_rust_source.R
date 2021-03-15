8db199566b0d6c6251319b00d92700bb163d24c5
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-15 17:02:24 +0200

    'pretty_rel_path' works correctly with no package directory

--- R/track_rust_source.R
+++ R/track_rust_source.R
@@ -1,3 +1,23 @@
+#' Returns `default` value if `expr` throws an error.
+#'
+#' This allows to silently handle errors and return a pre-defined
+#' default value.
+#' @param expr An expression to invoke (can be anything).
+#' @param default Value to return if `expr` throws an error.
+#' @return Either the result of `expr` or `default`. No
+#' type checks or type coersions performed.
+#' @example
+#' on_error_return_default(stop("This will be consumed"), "You get this instead")
+#' @noRd
+on_error_return_default <- function(expr, default = NULL) {
+  tryCatch(
+    expr,
+    error = function(e) {
+      default
+    }
+  )
+}
+
 # Converts any path to path, relative to the package root
 # E.g., outer_root/some_folder/code/packages/my_package/src/rust/src/lib.rs
 # becomes src/rust/src/lib.rs.
@@ -7,23 +27,33 @@
 # @param search_from Path from which package root is looked up.
 # @returns `path`, relative to the package root.
 pretty_rel_path <- function(path, search_from = ".") {
-  # Absolute path to the package root
-  package_root <- normalizePath(
-    rprojroot::find_package_root_file(path = search_from),
-    winslash = "/"
-  )
+  # Absolute path to the package root.
+  # If package root cannot be identified,
+  # an error is thrown, which gets converted into
+  # `""` using `on_error_return_default`.
+  package_root <-
+    on_error_return_default(
+      normalizePath(
+        rprojroot::find_package_root_file(path = search_from),
+        winslash = "/"
+      ),
+      default = ""
+    )
 
   # Absolute path.
   # `path` may not exist, so `mustWork` suppresses unnecessary warnings.
   path <- normalizePath(path, winslash = "/", mustWork = FALSE)
 
-  # If `package_root` is not parent of `path`,
-  # return `path` unchanged (for simplicity)
-  if (!stringi::stri_detect_fixed(
-    str = path,
-    pattern = package_root,
-    case_insensitive = TRUE
-  )) {
+  # If `package_root` is empty or not a parent of `path`,
+  # return `path` unchanged (for simplicity).
+  if (
+    !nzchar(package_root) ||
+      !stringi::stri_detect_fixed(
+        str = path,
+        pattern = package_root,
+        case_insensitive = TRUE
+      )
+  ) {
     return(path)
   }
 
