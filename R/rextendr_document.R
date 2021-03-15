3a23b0b4ebc63b22bbd3be9d9cd2dc149e4e5d4c
Claus Wilke <wilke@austin.utexas.edu>
2021-03-15 16:15:26 -0500

    minor improvements

--- R/rextendr_document.R
+++ R/rextendr_document.R
@@ -1,9 +1,9 @@
 #' Compile Rust code and generate package documentation.
 #'
-#' The function [`rextendr::document()`] updates the package documentation for an
+#' The function `rextendr::document()` updates the package documentation for an
 #' R package that uses `extendr` code, taking into account any changes that were
-#' made in the Rust code. It is a wrapper for [`devtools::document()`], and it
-#' executes `extendr`-specific routines before calling [`devtools::document()`].
+#' made in the Rust code. It is a wrapper for [devtools::document()], and it
+#' executes `extendr`-specific routines before calling [devtools::document()].
 #' Specifically, it ensures that Rust code is recompiled (when necessary) and that
 #' up-to-date R wrappers are generated before re-generating the package documentation.
 #' @inheritParams devtools::document
