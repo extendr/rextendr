daba7428d487c43ab57d681401ccabb8739f1f0c
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-22 17:19:47 +0200

    Fixing docs [skip ci]

--- R/write_file.R
+++ R/write_file.R
@@ -1,13 +1,14 @@
 #' Writes text to file
 #'
 #' This function is a wrapper around [`brio::write_lines()`].
-#' It also supports verboe output, similar to [`usethis::write_over()`],
+#' It also supports verbose output, similar to [`usethis::write_over()`],
 #' controlled by the `quiet` parameter.
 #' @param text Character vector containing text to write.
 #' @param path A string giving the file path to write to.
-#' @param search_root_from A string giving path to a package subfolder,
-#' which is used to find package root and produce message for the user.
-#' Unused if `quiet == TRUE`.
+#' @param search_root_from This parameter only affects messages displayed to the user. 
+#' It has no effect on where the file is written.
+#' It gets passed to [`pretty_rel_path()`] if `quiet = FALSE`.
+#' It is unused otherwise.
 #' @param quiet Logical scalar indicating whether the output should be quiet (`TRUE`)
 #'   or verbose (`FALSE`).
 #' @return The output of [`brio::write_lines()`] (invisibly).
