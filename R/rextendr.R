beebf7b4fde69fbb17ac2b5303adbeff7f085f94
Hiroaki Yutani <yutani.ini@gmail.com>
2021-12-15 08:52:20 +0900

    Mark "rextendr" topic as internal (#159)

--- R/rextendr.R
+++ R/rextendr.R
@@ -3,11 +3,13 @@
 #' The rextendr package implements functions to interface with Rust code from R.
 #' See [rust_source()] for details.
 #' @name rextendr
+#' @keywords internal
+"_PACKAGE"
+
 #' @importFrom dplyr   mutate %>%
 #' @importFrom purrr   map2 map2_chr map_lgl flatten_chr map_if every map discard
 #' @importFrom glue    glue glue_collapse
 #' @importFrom rlang   dots_list names2 as_function is_missing is_atomic is_null
 #' @importFrom rlang   is_na .data .env
 #' @importFrom stringi stri_replace_all_regex
-#' @docType package
 NULL
