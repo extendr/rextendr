215cdddbc38475656ae6846644f88b0f6d8c50c9
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2022-05-04 18:35:19 +0300

    Extracting helper methods

--- R/source.R
+++ R/source.R
@@ -274,37 +274,7 @@ invoke_cargo <- function(toolchain, specific_target, dir, profile,
 
   message_buffer <- character(0)
   env <- rlang::current_env()
-
-  tty_has_colors <- isTRUE(cli::num_ansi_colors() > 1L)
-
-  if (identical(.Platform$OS.type, "windows")) {
-    # On Windows, PATH to Rust toolchain should be set by the installer.
-    # If R >= 4.2, we need to override the linker setting.
-    if (identical(R.version$crt, "ucrt")) {
-      # libgcc_mock is created in configure.ucrt on installation.
-      libgcc_path <- system.file("libgcc_mock", package = "rextendr")
-      if (identical(libgcc_path, "")) {
-        ui_throw(
-          "Unable to find {.file inst/libgcc_mock}. Please reinstall the rextendr package",
-        )
-      }
-
-      cargo_envvars <- c("current",
-        CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "x86_64-w64-mingw32.static.posix-gcc.exe",
-        LIBRARY_PATH = paste0(libgcc_path, ";", Sys.getenv("LIBRARY_PATH"))
-      )
-    } else {
-      cargo_envvars <- NULL
-    }
-  } else {
-    # In some environments, ~/.cargo/bin might not be included in PATH, so we need
-    # to set it here to ensure cargo can be invoked. It's added to the tail as a
-    # fallback, which is used only when cargo is not found in the user's PATH.
-    path_envvar <- Sys.getenv("PATH", unset = "")
-    cargo_path <- path.expand("~/.cargo/bin")
-    # "current" means appending or overwriting the envvars in addition to the current ones.
-    cargo_envvars <- c("current", PATH = glue("{path_envvar}:{cargo_path}"))
-  }
+  cargo_envvars <- get_cargo_envvars()
 
   compilation_result <- processx::run(
     command = "cargo",
@@ -317,7 +287,7 @@ invoke_cargo <- function(toolchain, specific_target, dir, profile,
       glue("--target-dir={file.path(dir, 'target')}"),
       if (profile == "release") "--release" else NULL,
       "--message-format=json-diagnostic-rendered-ansi",
-      if (tty_has_colors) {
+      if (tty_has_colors()) {
         "--color=always"
       } else {
         "--color=never"
@@ -334,7 +304,7 @@ invoke_cargo <- function(toolchain, specific_target, dir, profile,
     env = cargo_envvars
   )
 
-  check_cargo_output(compilation_result, message_buffer, tty_has_colors, quiet)
+  check_cargo_output(compilation_result, message_buffer, tty_has_colors(), quiet)
 }
 
 #' Gathers ANSI-formatted cargo output
