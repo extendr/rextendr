3d6f9824b98ed6f2fdb9ed0354d06b37b504c295
Mossa <cgmossa@gmail.com>
2022-05-11 13:16:49 +0200

    Minor edit

--- R/track_rust_source.R
+++ R/track_rust_source.R
@@ -18,14 +18,15 @@ on_error_return_default <- function(expr, default = NULL) {
   )
 }
 
-# Converts any path to path, relative to the package root
-# E.g., outer_root/some_folder/code/packages/my_package/src/rust/src/lib.rs
-# becomes src/rust/src/lib.rs.
-# Used for pretty printing.
-# Assumes that `path` is within `package_root`.
-# @param path Scalar path to format.
-# @param search_from Path from which package root is looked up.
-# @returns `path`, relative to the package root.
+#' Converts any path to path, relative to the package root
+#' E.g., outer_root/some_folder/code/packages/my_package/src/rust/src/lib.rs
+#' becomes src/rust/src/lib.rs.
+#' Used for pretty printing.
+#' Assumes that `path` is within `package_root`.
+#' @param path Scalar path to format.
+#' @param search_from Path from which package root is looked up.
+#' @returns `path`, relative to the package root.
+#' @noRd
 pretty_rel_path <- function(path, search_from = ".") {
   # Absolute path to the package root.
   # If package root cannot be identified,
@@ -167,15 +168,16 @@ needs_compilation <- function(path = ".", quiet = getOption("usethis.quiet", FAL
   TRUE
 }
 
-# Equivalent to `fs::file_info`.
-#
-# Takes paths, retrieves info using `file.info`
-# and converts obtained `data.frame` in a more suitable `tibble`
-# with `path` column representing file paths (instead of row names).
-#
-# @param path File paths to inspect (accepts multiple value).
-# @returns A `tibble::tibble()` with information about files,
-# including `path` and `mtime`, which are used elsewhere in `rextendr`.
+#' Equivalent to `fs::file_info`.
+#'
+#' Takes paths, retrieves info using `file.info`
+#' and converts obtained `data.frame` in a more suitable `tibble`
+#' with `path` column representing file paths (instead of row names).
+#'
+#' @param path File paths to inspect (accepts multiple value).
+#' @returns A `tibble::tibble()` with information about files,
+#' including `path` and `mtime`, which are used elsewhere in `rextendr`.
+#' @noRd
 get_file_info <- function(path) {
   # We do not need extra columns, only `mtime`
   info <- file.info(path, extra_cols = FALSE)
