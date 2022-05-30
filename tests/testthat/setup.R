ca41ec0126e8eb0404a666caa81ba78f5cdddf92
Hiroaki Yutani <yutani.ini@gmail.com>
2022-05-30 20:37:02 +0900

    Invoke ./configure.ucrt to ensure inst/libgcc_mock exists (#199)
    * Invoke `./configure.ucrt` to ensure `inst/libgcc_mock` exists
    
    * Do it quietly
    
    * Tweak comment

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,7 @@
 old_test_settings <- options(
   usethis.quiet = TRUE
 )
+
+# Ensure inst/libgcc_mock exists (Note that `quiet = TRUE` seems mandatory here,
+# otherwise it crashes probably because of broken pipe?)
+pkgbuild::compile_dll(force = TRUE, quiet = TRUE)
