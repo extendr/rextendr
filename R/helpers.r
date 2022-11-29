600944597472f2bf34fb7b1e1acbe0ff2536db19
Hiroaki Yutani <yutani.ini@gmail.com>
2022-11-30 08:37:39 +0900

    Just touch libgcc_eh.a instead of actually compiling it (#218)
    * Just touch libgcc_eh.a instead of actually compiling it
    
    * Add comments
    
    * Update comment
    
    * Update snapshot

--- R/helpers.r
+++ R/helpers.r
@@ -5,13 +5,15 @@ get_cargo_envvars <- function() {
     # On Windows, PATH to Rust toolchain should be set by the installer.
     # If R >= 4.2, we need to override the linker setting.
     if (identical(R.version$crt, "ucrt")) {
-      # libgcc_mock is created in configure.ucrt on installation.
-      libgcc_path <- system.file("libgcc_mock", package = "rextendr")
-      if (identical(libgcc_path, "")) {
-        ui_throw(
-          "Unable to find {.file inst/libgcc_mock}. Please reinstall the rextendr package",
-        )
-      }
+      # `rustc` adds `-lgcc_eh` flags to the compiler, but Rtools' GCC doesn't have
+      # `libgcc_eh` due to the compilation settings. So, in order to please the
+      # compiler, we need to add empty `libgcc_eh` to the library search paths.
+      #
+      # For more details, please refer to
+      # https://github.com/r-windows/rtools-packages/blob/2407b23f1e0925bbb20a4162c963600105236318/mingw-w64-gcc/PKGBUILD#L313-L316
+      libgcc_path <- file.path(system.file(package = "rextendr"), "libgcc_mock")
+      dir.create(libgcc_path, showWarnings = FALSE)
+      file.create(file.path(libgcc_path, "libgcc_eh.a"))
 
       cargo_envvars <- c("current",
         CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "x86_64-w64-mingw32.static.posix-gcc.exe",
