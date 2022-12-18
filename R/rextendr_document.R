8601c74b1c02f5989cfb1b2676beacb0e3adaa6f
Hiroaki Yutani <yutani.ini@gmail.com>
2022-12-18 15:34:32 +0900

    Deprecate document()

--- R/rextendr_document.R
+++ R/rextendr_document.R
@@ -1,21 +1,16 @@
 #' Compile Rust code and generate package documentation.
 #'
-#' The function `rextendr::document()` updates the package documentation for an
-#' R package that uses `extendr` code, taking into account any changes that were
-#' made in the Rust code. It is a wrapper for [devtools::document()], and it
-#' executes `extendr`-specific routines before calling [devtools::document()].
-#' Specifically, it ensures that Rust code is recompiled (when necessary) and that
-#' up-to-date R wrappers are generated before re-generating the package documentation.
+#' `r lifecycle::badge("superseded")`
+#'
+#' `rextendr::document()` was a wrapper for [devtools::document()] to ensure
+#' that Rust code is recompiled (when necessary) and that up-to-date R wrappers
+#' are generated before re-generating the package documentation. However, as of
+#' pkgdown 1.4.0, `devtools::document()` detects the changes on Rust code, so
+#' the users can rely on it directly.
 #' @inheritParams devtools::document
 #' @return No return value, called for side effects.
 #' @export
 document <- function(pkg = ".", quiet = getOption("usethis.quiet", FALSE), roclets = NULL) {
-  try_save_all(quiet = quiet)
-
-  withr::local_envvar(devtools::r_env_vars())
-
-  register_extendr(path = pkg, quiet = quiet)
-
-  rlang::check_installed("devtools")
+  lifecycle::deprecate_soft("0.3.0", "rextendr::document()", "devtools::document()")
   devtools::document(pkg = pkg, roclets = roclets, quiet = FALSE)
 }
