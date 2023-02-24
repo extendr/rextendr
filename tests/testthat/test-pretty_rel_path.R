420c120e5c91f00cdfaffd8e75b3aad1ac238712
Hiroaki Yutani <yutani.ini@gmail.com>
2023-02-24 19:36:20 +0900

    Update tests/testthat/test-pretty_rel_path.R
    Co-authored-by: Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>

--- tests/testthat/test-pretty_rel_path.R
+++ tests/testthat/test-pretty_rel_path.R
@@ -34,8 +34,6 @@ test_that("Find relative path starting outside of package directory, return abso
   pkg_root <- local_package("testpkg")
   use_extendr()
 
-  r_version <- as.numeric_version(glue::glue("{R.version$major}.{R.version$minor}"))
-  is_windows <- .Platform$OS.type == "windows"
 
   expect_equal(
     pretty_rel_path(
