54badd335aedf3814a64a994ff94a9f656270505
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-03-06 00:18:31 +0200

    Add more tests

--- tests/testthat/test-optional-features.R
+++ tests/testthat/test-optional-features.R
@@ -1,6 +1,6 @@
-test_that("Feature 'either' is enabled when no extra dependencies are passed", {
+test_that("Feature 'ndarray' is enabled when no extra dependencies are specifiedd", {
   input <- file.path("../data/either_example.rs")
-  rextendr::rust_source(
+  rust_source(
     file = input,
     features = "ndarray"
   )
@@ -10,4 +10,73 @@ test_that("Feature 'either' is enabled when no extra dependencies are passed", {
   actual_sum <- matrix_sum(data)
 
   expect_equal(actual_sum, expected_sum)
+})
+
+test_that("Feature 'ndarray' is enabled when dependency is explicitly set", {
+  input <- file.path("../data/either_example.rs")
+  rust_source(
+    file = input,
+    features = "ndarray",
+    dependencies = list(ndarray = "*")
+  )
+
+  data <- matrix(runif(100L), 25)
+  expected_sum <- sum(data)
+  actual_sum <- matrix_sum(data)
+
+  expect_equal(actual_sum, expected_sum)
+})
+
+test_that("Feature 'ndarray' is enabled when dependency is explicitly set to a complex value", {
+  input <- file.path("../data/either_example.rs")
+  rust_source(
+    file = input,
+    features = "ndarray",
+    dependencies = list(ndarray = list(version = "*"))
+  )
+
+  data <- matrix(runif(100L), 25)
+  expected_sum <- sum(data)
+  actual_sum <- matrix_sum(data)
+
+  expect_equal(actual_sum, expected_sum)
+})
+
+test_that("Feature 'ndarray' is enabled when other dependencies are specified", {
+  input <- file.path("../data/either_example.rs")
+  rust_source(
+    file = input,
+    features = "ndarray",
+    dependencies = list(either = list(version = "*"))
+  )
+
+  data <- matrix(runif(100L), 25)
+  expected_sum <- sum(data)
+  actual_sum <- matrix_sum(data)
+
+  expect_equal(actual_sum, expected_sum)
+})
+
+test_that("Feature 'ndarray' is enabled when 'extendr-api' has features enabled", {
+  input <- file.path("../data/either_example.rs")
+  rust_source(
+    file = input,
+    features = "ndarray",
+    extendr_deps = list(`extendr-api` = list(version = "*", features = array("serde")))
+  )
+
+  data <- matrix(runif(100L), 25)
+  expected_sum <- sum(data)
+  actual_sum <- matrix_sum(data)
+
+  expect_equal(actual_sum, expected_sum)
+})
+
+test_that("Enable multiple features simultaneously", {
+  rust_function("fn test_multiple_features() {}", features = c("ndarray", "serde", "graphics"))
+  expect_no_error(test_multiple_features())
+})
+
+test_that("'features' accepts only pre-defined values", {
+  expect_error(rust_function("fn test_two_features() {}", features = "not-a-feature"))
 })
