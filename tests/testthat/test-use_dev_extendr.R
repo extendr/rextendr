9376cd282d20d7ea330a868f88f42d491bd743d1
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-03-14 22:45:59 +0200

    Align rextendr tests with dev extendr (#254)
    * Align rextendr tests with dev extendr
    
    * Trying to patch libR-sys

--- tests/testthat/test-use_dev_extendr.R
+++ tests/testthat/test-use_dev_extendr.R
@@ -1,10 +1,9 @@
 test_that("`use_dev_extendr = TRUE` works together with `features`", {
   rust_function(
-    "fn uses_either() -> either::Either<Rint, Rfloat> {either::Either::Left(Rint::from(42i32))}",
+    "fn uses_either() -> Either<Rint, Rfloat> { Either::Left(Rint::from(42i32)) }",
     features = "either",
     use_dev_extendr = TRUE,
-    quiet = TRUE, # Suppresses warnings while the feature is still experimental
-    dependencies = list(either = "*") # Crates associated with experimental features are not references automatically
+    quiet = TRUE # Suppresses warnings while the feature is still experimental
   )
 
   expect_equal(42L, uses_either())
