6db46635208a87c0428b12d5ae2d10b2bf2e0481
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-03-19 14:59:17 +0200

    Fix test

--- tests/testthat/test-extendr_function_options.R
+++ tests/testthat/test-extendr_function_options.R
@@ -1,16 +1,15 @@
 test_that("`extendr` code is compiled with `either` feature and `use_try_from` enabled", {
   rust_function(
-    "fn type_aware_sum(input : either::Either<Integers, Doubles>) -> either::Either<Rint, Rfloat> {
+    "fn type_aware_sum(input : Either<Integers, Doubles>) -> Either<Rint, Rfloat> {
       match input {
-        either::Either::Left(left) => either::Either::Left(left.iter().sum()),
-        either::Either::Right(right) => either::Either::Right(right.iter().sum())
+        Either::Left(left) => Either::Left(left.iter().sum()),
+        Either::Right(right) => Either::Right(right.iter().sum())
       }
     }",
     extendr_fn_options = list("use_try_from" = TRUE),
     features = "either",
     use_dev_extendr = TRUE,
-    quiet = TRUE, # Suppresses warnings while the feature is still experimental
-    dependencies = list(either = "*") # Crates associated with experimental features are not references automatically
+    quiet = TRUE # Suppresses warnings while the feature is still experimental
   )
 
   int_sum <- type_aware_sum(1:5)
