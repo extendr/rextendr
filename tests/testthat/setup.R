b6eec1a72bd0c76aad7993f26fb688bbd9fa994d
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-03-25 20:20:05 +0200

    Remove pkg building hack

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,5 +1,1 @@
 withr::local_options(usethis.quiet = TRUE, .local_envir = teardown_env())
-
-# Ensure inst/libgcc_mock exists (Note that `quiet = TRUE` seems mandatory here,
-# otherwise it crashes probably because of broken pipe?)
-pkgbuild::compile_dll(force = TRUE, quiet = TRUE)
