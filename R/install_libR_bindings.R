46c14d3380b9297528b19c5ccfc771ba27b6c860
Josiah Parry <josiah.parry@gmail.com>
2023-04-17 08:49:25 -0400

    change  to {.code force = TRUE}

--- R/install_libR_bindings.R
+++ R/install_libR_bindings.R
@@ -24,7 +24,7 @@ install_libR_bindings <- function(version = "*", force = FALSE, quiet = FALSE, p
 
   bindings_file <- file.path(package_dir, "rust", "libR-sys", "src", "bindings.rs")
   if (!isTRUE(force) && isTRUE(file.exists(bindings_file))) {
-    cli::cli_alert_info("libR bindings are already installed. Rerun with `force = TRUE` to force re-install.")
+    cli::cli_alert_info("libR bindings are already installed. Rerun with {.code force = TRUE} to force re-install.")
     return(invisible())
   }
 
