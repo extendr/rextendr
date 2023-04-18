da6fde103145e3672ad8a177d51d5cc25d2a2dce
Josiah Parry <josiah.parry@gmail.com>
2023-04-18 09:55:09 -0400

    address https://github.com/extendr/rextendr/pull/263#discussion_r1170065965

--- R/features.R
+++ R/features.R
@@ -5,9 +5,7 @@ features_config <- rlang::env(
 validate_extendr_features <- function(features, suppress_warnings) {
   features <- features %||% character(0)
 
-  is_char <- try(vctrs::vec_assert(features, character()), silent = TRUE)
-
-  if (inherits(is_char, "try-error")) {
+  if (!vctrs::vec_is(features, character())) {
     cli::cli_abort(c(
       "!"= "{.arg features} must be a vector with type {.cls character}",
       "Instead, it has type {.cls {class(features)}}."
