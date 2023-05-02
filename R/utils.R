0daf15f9fd8f3de5c732e4191d1fd995d0522504
eitsupi <ts1s1andn@gmail.com>
2023-05-02 14:44:32 +0000

    docs: ensure not export

--- R/utils.R
+++ R/utils.R
@@ -36,6 +36,7 @@ local_quiet_cli <- function(quiet, env = rlang::caller_env()) {
 #' Helper function for check cargo commands.
 #' @param args Character vector, arguments to the `cargo` command. Pass to [processx::run()]'s args param.
 #' @return a logical indicating if the command was available.
+#' @noRd
 cargo_command_available <- function(args = "--help") {
   if (processx::run("cargo", args, error_on_status = FALSE)$status == 0L) {
     is_available <- TRUE
