1ad658118c62f8c88287649c4c503025ed4e0033
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2023-11-19 20:12:00 +0200

    Move test to the shared file

--- tests/testthat/test-use_extendr_with_path.R
+++
@@ -1,8 +0,0 @@
-test_that("`use_extendr()` works correctly when path is specified explicitly", {
-  skip_if_not_installed("usethis")
-  local_temp_dir("temp_dir")
-  usethis::create_package("testpkg")
-
-  use_extendr(path = "testpkg")
-  succeed()
-})
