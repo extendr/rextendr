d4af603f20c392ddafca746b114afbaf2ca70561
kbvernon <kenneth.b.vernon@gmail.com>
2024-09-22 16:45:05 -0600

    draft and test

---
+++ R/read_cargo_metadata.R
@@ -0,0 +1,44 @@
+#' Retrieve metadata for packages and workspaces
+#'
+#' @param path character scalar, the R package directory
+#'
+#' @details
+#' For more details, see
+#' \href{https://doc.rust-lang.org/cargo/commands/cargo-metadata.html}{Cargo docs}
+#' for `cargo-metadata`. See especially "JSON Format" to get a sense of what you
+#' can expect to find in the returned list.
+#'
+#' @return `list`, including the following elements:
+#' - "packages"
+#' - "workspace_members"
+#' - "workspace_default_members"
+#' - "resolve"
+#' - "target_directory"
+#' - "version"
+#' - "workspace_root"
+#' - "metadata"
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' read_cargo_metadata()
+#' }
+#'
+read_cargo_metadata <- function(path = ".") {
+  root <- rprojroot::find_package_root_file(path = path)
+
+  rust_folder <- normalizePath(
+    file.path(root, "src", "rust"),
+    winslash = "/",
+    mustWork = FALSE
+  )
+
+  out <- processx::run(
+    "cargo",
+    args = c("metadata", "--format-version=1", "--no-deps"),
+    wd = rust_folder
+  )
+
+  jsonlite::fromJSON(out[["stdout"]])
+}
---
+++ tests/testthat/test-read_cargo_metadata.R
@@ -0,0 +1,14 @@
+test_that("read_cargo_metadata() returns package or workspace metadata", {
+  skip_if_not_installed("usethis")
+
+  path <- local_package("testpkg")
+
+  # capture setup messages
+  withr::local_options(usethis.quiet = FALSE)
+
+  use_extendr(path, quiet = TRUE)
+
+  out <- read_cargo_metadata()
+
+  expect_s3_class(out, "list")
+})
