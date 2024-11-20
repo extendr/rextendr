d8689d9242f6da8c919da72589c04630fcd108a1
Kenneth Blake Vernon <53311626+kbvernon@users.noreply.github.com>
2024-11-19 18:37:59 -0700

    internal utilities to find rust crate and cargo manifest (#399)
    * internal functions and tests
    
    * Update R/find_extendr.R
    
    Co-authored-by: Josiah Parry <josiah.parry@gmail.com>
    
    * Update R/find_extendr.R
    
    Co-authored-by: Josiah Parry <josiah.parry@gmail.com>
    
    ---------
    
    Co-authored-by: Josiah Parry <josiah.parry@gmail.com>

---
+++ R/find_extendr.R
@@ -0,0 +1,69 @@
+#' Get path to Rust crate in R package directory
+#'
+#' @param path character scalar, the R package directory
+#' @param error_call call scalar, from rlang docs: "the defused call with which
+#' the function running in the frame was invoked"
+#'
+#' @return character scalar, path to Rust crate
+#'
+#' @examples
+#' \dontrun{
+#' find_extendr_crate()
+#' @keywords internal
+#' @noRd
+#' }
+find_extendr_crate <- function(
+    path = ".",
+    error_call = rlang::caller_call()) {
+  check_character(path, call = error_call, class = "rextendr_error")
+
+  rust_folder <- rprojroot::find_package_root_file(
+    "src", "rust",
+    path = path
+  )
+
+  if (!dir.exists(rust_folder)) {
+    cli::cli_abort(
+      "Could not find Rust crate at {.path rust_folder}.",
+      call = error_call,
+      class = "rextendr_error"
+    )
+  }
+
+  rust_folder
+}
+
+#' Get path to Cargo manifest in R package directory
+#'
+#' @param path character scalar, the R package directory
+#' @param error_call call scalar, from rlang docs: "the defused call with which
+#' the function running in the frame was invoked"
+#'
+#' @return character scalar, path to Cargo manifest
+#'
+#' @examples
+#' \dontrun{
+#' find_extendr_manifest()
+#' @keywords internal
+#' @noRd
+#' }
+find_extendr_manifest <- function(
+    path = ".",
+    error_call = rlang::caller_call()) {
+  check_character(path, call = error_call, class = "rextendr_error")
+
+  manifest_path <- rprojroot::find_package_root_file(
+    "src", "rust", "Cargo.toml",
+    path = path
+  )
+
+  if (!file.exists(manifest_path)) {
+    cli::cli_abort(
+      "Could not find Cargo manifest at {.path manifest_path}.",
+      call = error_call,
+      class = "rextendr_error"
+    )
+  }
+
+  manifest_path
+}
