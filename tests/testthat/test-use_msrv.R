1601fa1fceb01d56de771435cb7d8f10b35d9da3
Josiah Parry <josiah.parry@gmail.com>
2024-11-25 03:19:05 -0500

    increase code coverage (#405)

--- tests/testthat/test-use_msrv.R
+++ tests/testthat/test-use_msrv.R
@@ -7,12 +7,22 @@ test_that("use_msrv() modifies the MSRV in the DESCRIPTION", {
   withr::local_options(usethis.quiet = FALSE)
 
   use_extendr(path, quiet = TRUE)
-  use_msrv("1.70", path)
+  expect_no_error(use_msrv("1.70", path))
 
   d <- desc::desc("DESCRIPTION")
 
   expect_identical(
     "Cargo (Rust's package manager), rustc >= 1.70",
     d$get_field("SystemRequirements")
+  )
+
+  expect_error(use_msrv("adksfghu", path))
+
+  expect_error(use_msrv("1.70", path = "../doesntexist"))
+
+  # when overwrite is FALSE and SystemRequirements is already set
+  expect_message(
+    use_msrv("1.65", overwrite = FALSE),
+    "The SystemRequirements field in the "
   )
 })
