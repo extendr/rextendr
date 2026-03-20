1b9cf8b398813b67ea9322a28012e9322efd74e2
Blake Vernon <kenneth.b.vernon@gmail.com>
2026-03-20 09:09:33 -0600

    register panic hook

--- inst/templates/entrypoint.c
+++ inst/templates/entrypoint.c
@@ -2,7 +2,9 @@
 // to avoid the linker removing the static library.
 
 void R_init_{{{mod_name}}}_extendr(void *dll);
+void register_extendr_panic_hook(void);
 
 void R_init_{{{mod_name}}}(void *dll) {
+    register_extendr_panic_hook();
     R_init_{{{mod_name}}}_extendr(dll);
 }
